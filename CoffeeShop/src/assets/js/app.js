29ecb0adddf4622f6ad424b11f5f8b7870450c0e
Brooks Von Doom <jeffery.brooks@loop.colum.edu>
2017-11-20 22:02:37 -0600

    page style modifications

--- CoffeeShop/src/assets/js/app.js
+++ CoffeeShop/src/assets/js/app.js
@@ -3,6 +3,11 @@ import whatInput from 'what-input';
 
 window.$ = $;
 
+// SWEET ALERT 2
+import swal from 'sweetalert2/dist/sweetalert2.all.min.js'
+
+swal("Hi from webpack");
+
 import Foundation from 'foundation-sites';
 // If you want to pick and choose which modules to include, comment out the above and uncomment
 // the line below
